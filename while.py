cd3f4efbccdcd1d75b503858fb2e3a785915f986
chetana7483 <167082554+chetana7483@users.noreply.github.com>
2024-04-17 22:53:48 +0530

    Create while.py

---
+++ while.py
@@ -0,0 +1,27 @@
+i=56
+while i<=96:
+    print(i)
+    i=i+1
+
+i=1222
+while i>=995:
+    print(i)
+    i-=1  
+
+
+i=1
+while i<=10:
+        print(i*5)
+        i+=1
+        
+i=10
+while i>=1:
+     print(i*7)
+     i-=1
+
+i=12
+while i<=56:
+    if i % 2==0:
+        print(i)
+    i+=1        
+
