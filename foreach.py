c8c22567e135363e690b06df6d5f937079a014ec
chetana7483 <167082554+chetana7483@users.noreply.github.com>
2024-04-17 22:52:31 +0530

    Create foreach.py

---
+++ foreach.py
@@ -0,0 +1,13 @@
+//for each program
+allkeys=store.keys()
+print(allkeys)
+for ele in allkeys:
+    print(ele)
+for ele in allkeys:
+    print(ele)
+ for ch in "abcdef":
+    print(ch)
+for val in[12,23,34,45,67,78]:
+    print(val)
+
+
