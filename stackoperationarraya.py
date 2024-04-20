6230e75dd40951452fc5ec10fa8cdb3180de32cc
chetana7483 <167082554+chetana7483@users.noreply.github.com>
2024-04-20 15:13:21 +0530

    Create stackoperationarraya.py

---
+++ stackoperationarraya.py
@@ -0,0 +1,73 @@
+class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None 
+ 
+def push(stack, val):
+    stack.insert(0, val)
+    print(val, "inserted")
+ class Node:
+    def __init__(self, data):
+        self.data = data 
+        self.next = None 
+ 
+def pop(stack):
+    if len(stack) == 0:
+        print("Stack is empty, can't delete anything")
+        return 
+ 
+    print(stack[0], "deleted")
+    stack.pop(0)
+ 
+def printStack(stack):
+    if len(stack) == 0:
+        print("Stack is empty, nothing to print")
+        return 
+    print(stack)
+ 
+ 
+stack = []
+push(stack, 10)
+push(stack, 20)
+push(stack, 30)
+push(stack, 40)
+push(stack, 50)
+push(stack, 60)
+ 
+# 60 --> 50 --> 40 --> 30 --> 20 --> 10 
+ 
+ 
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+pop(stack)
+printStack(stack)
+ 
+ 
+ 
+ 
+ 
+ 
+  
+
+
+
+ 
+ 
